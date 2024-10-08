627521ab6db1235dec3dcdbb5f4176e80ac0beba
huyn3195 <98776668+huyn3195@users.noreply.github.com>
2024-10-07 22:07:48 -0500

    First commit

---
+++ client/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+ReactDOM.render(<h1>Hello, World!</h1>, document.getElementById("root"));
